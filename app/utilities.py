d182a263cd5fb42a9a74e93d56a4691d46036e3f
Mouse Reeve <mousereeve@riseup.net>
2018-01-16 13:10:59 -0500

    Fixes a/an regex for words like "university"

--- app/utilities.py
+++ app/utilities.py
@@ -3,7 +3,8 @@ import re
 
 def format_text(text):
     ''' fix a/an type issues in tracery '''
-    return re.sub(r'\b([Aa]) ([aeiouAEIOU])', r'\1n \2', text)
+    return re.sub(r'\b([aA]) ((?![uU](?![nN](?![iI])))[aeiouAEIOU])',
+                  r'\1n \2', text)
 
 
 def get_latin(word, capitalize=False):
